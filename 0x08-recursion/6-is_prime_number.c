b86054786d2adc09001b1686395e40e9fdaae570
Romanyerom <Romanyerom@github.com>
2023-06-07 07:43:34 -0700

    redo

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,35 +3,35 @@
 /**
  * is_prime_number - checks whether a number is prime or not
  * @n: number to check
+ * is_divisible - Checks if n is divisible by divisor.
+ * @n: The number to be checked for divisibility.
+ * @divisor: The divisor to check against.
+ *
+ * Return: 1 if n is divisible by divisor, 0 otherwise.
  *
  * Return: 1 if n is prime, 0 otherwise
  */
 
+int is_divisible(int n, int divisor)
+{
+	if (divisor == 1)
+	{
+		return (0);  /* Not a prime number */
+	}
+
+	if (n % divisor == 0)
+	{
+		return (1);  /* Divisible, not a prime number */
+	}
+
+	return (is_divisible(n, divisor - 1));
+}
 int is_prime_number(int n)
 {
-	/* Base cases */
 	if (n <= 1)
 	{
-		return (0);
-		/* Not a prime number */
-	}
-	else if (n <= 3)
-	{
-		return (1);
-		/* 2 and 3 are prime numbers */
+		return (0);  /* Not a prime number */
 	}
 
-	/* Check if n is divisible by numbers from 2 to sqrt(n) */
-	int i;
-
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-			/* Not a prime number */
-		}
-	}
-
-	return (1);  /* Prime number */
+	return (!is_divisible(n, n - 1));
 }
